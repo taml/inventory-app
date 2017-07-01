fa5b3b6bab254d97c362592cd52c1bc0dd2e2e87
Tam Love <dark_angel4eva@email.com>
2017-07-01 22:50:50 +0100

    Added cursor adapter and loaders.

--- app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/InventoryActivity.java
@@ -1,13 +1,82 @@
 package com.example.android.inventoryapp;
 
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
 
-public class InventoryActivity extends AppCompatActivity {
+import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
+
+
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int INVENTORY_LOADER = 0;
+    InventoryCursorAdapter mInventoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        // Find GridView to populate
+        GridView gridViewItems = (GridView) findViewById(R.id.inventory_grid_view);
+        // Setup cursor adapter using cursor from last step
+        mInventoryAdapter = new InventoryCursorAdapter(this, null);
+        // Attach cursor adapter to the ListView
+        gridViewItems.setAdapter(mInventoryAdapter);
+
+        getSupportLoaderManager().initLoader(INVENTORY_LOADER, null, this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_inventory, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Add" menu option
+            case R.id.add_item:
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.delete_all_items:
+                deleteAllInventoryItems();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {InventoryEntry._ID, InventoryEntry.COLUMN_NAME,
+                InventoryEntry.COLUMN_PRICE, InventoryEntry.COLUMN_QUANTITY};
+        return new CursorLoader(this, InventoryEntry.CONTENT_URI,
+                projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mInventoryAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mInventoryAdapter.swapCursor(null);
+    }
+
+    private void deleteAllInventoryItems() {
+        int rowsDeleted = getContentResolver().delete(InventoryEntry.CONTENT_URI, null, null);
+        Log.v("InventoryActivity", rowsDeleted + " rows deleted from inventory database");
     }
 }
---
+++ app/src/main/java/com/example/android/inventoryapp/InventoryCursorAdapter.java
@@ -0,0 +1,67 @@
+package com.example.android.inventoryapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
+
+public class InventoryCursorAdapter extends CursorAdapter{
+
+    /**
+     * Constructs a new {@link InventoryCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank grid item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.grid_item, parent, false);
+    }
+
+    /**
+     * This method binds the inventory data (in the current row pointed to by cursor) to the given
+     * grid item layout. For example, the name for the current item can be set on the name TextView
+     * in the grid item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView nameTextView = (TextView) view.findViewById(R.id.item_name);
+        TextView priceTextView = (TextView) view.findViewById(R.id.item_price);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.item_quantity);
+
+        int nameColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_QUANTITY);
+
+        String name = cursor.getString(nameColumnIndex);
+        String price = cursor.getString(priceColumnIndex);
+        String quantity = cursor.getString(quantityColumnIndex);
+
+        nameTextView.setText(name);
+        priceTextView.setText(price);
+        quantityTextView.setText(quantity);
+    }
+}
