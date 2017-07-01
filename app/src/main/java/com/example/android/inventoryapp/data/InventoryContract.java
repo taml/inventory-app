167e04f34d8354b7355618353ce3b8979b61b716
Tam Love <dark_angel4eva@email.com>
2017-07-01 21:34:51 +0100

    Added database contract, helper and provider.

---
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryContract.java
@@ -0,0 +1,62 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class InventoryContract {
+
+    //An empty private constructor makes sure that the class is not going to be initialised.
+    private InventoryContract(){
+    }
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.inventoryapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_INVENTORY = "inventory";
+
+    public static abstract class InventoryEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of inventory.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single inventory item.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+
+        public static final String TABLE_NAME = "inventory";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_IMAGE = "image";
+        public static final String COLUMN_SUPPLIER_NAME = "supplier";
+        public static final String COLUMN_SUPPLIER_EMAIL = "email";
+
+        /**
+         * Possible inventory suppliers.
+         */
+        public static final int SUPPLIER_QUILL_LONDON = 0;
+        public static final int SUPPLIER_SCRIBBLERS = 1;
+        public static final int SUPPLIER_CULT_PENS = 2;
+        public static final int SUPPLIER_JET_PENS = 3;
+
+        /**
+         * Returns whether or not the given supplier is {@link #SUPPLIER_QUILL_LONDON}, {@link #SUPPLIER_SCRIBBLERS},
+         * {@link #SUPPLIER_CULT_PENS}, or {@link #SUPPLIER_JET_PENS}.
+         */
+        public static boolean isValidSupplier(int supplier) {
+            if (supplier == SUPPLIER_QUILL_LONDON || supplier == SUPPLIER_SCRIBBLERS || supplier == SUPPLIER_CULT_PENS || supplier == SUPPLIER_JET_PENS) {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
@@ -0,0 +1,42 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
+
+public class InventoryDbHelper extends SQLiteOpenHelper{
+
+    public static final String LOG_TAG = InventoryDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "inventory.db";
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link InventoryDbHelper}.
+     *
+     * @param context of the app
+     */
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_INVENTORY_TABLE = "CREATE TABLE " + InventoryEntry.TABLE_NAME + " (" +
+                InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                InventoryEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                InventoryEntry.COLUMN_PRICE + " REAL NOT NULL DEFAULT 0.00, " +
+                InventoryEntry.COLUMN_QUANTITY + " INTEGER NOT NULL DEFAULT 0, " +
+                InventoryEntry.COLUMN_IMAGE + " BLOB, " +
+                InventoryEntry.COLUMN_SUPPLIER_NAME + " TEXT NOT NULL, " +
+                InventoryEntry.COLUMN_SUPPLIER_EMAIL + " TEXT NOT NULL);";
+        Log.v(LOG_TAG,SQL_CREATE_INVENTORY_TABLE);
+        db.execSQL(SQL_CREATE_INVENTORY_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
